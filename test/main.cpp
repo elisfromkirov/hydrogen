13dff3e2020c29d8f03383137283f67ea2fd9bae
Moskalev Nikita <mozgg2002@gmail.com>
2023-02-09 13:47:01 +0300

    add compiler warnings and different build variants to cmake

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,14 @@
 #include <hydrogen/fault/detail/runtime/get_scheduler.hpp>
+#include <hydrogen/fault/detail/runtime/mutex.hpp>
 #include <hydrogen/fault/detail/runtime/thread.hpp>
 
 #include <iostream>
 
 using namespace hydrogen::detail;
 
-int main() {
+void YieldExample() {
+  std::cout << "yield example" << std::endl;
+
   Thread main_thread([]() {
     Thread thread_1([]() {
       std::cout << "#1" << std::endl;
@@ -21,5 +24,46 @@ int main() {
     thread_2.Join();
   });
   GetScheduler()->Dispatch();
+
+  std::cout << std::endl;
+}
+
+void MutexExample() {
+  std::cout << "mutex example" << std::endl;
+
+  Thread main_thread([]() {
+    Mutex mutex{};
+    crutch::SizeType number{0};
+
+    Thread thread_1([&]() {
+      for (crutch::SizeType i = 0; i < 100; ++i) {
+        mutex.Lock();
+        number += 2;
+        mutex.Unlock();
+      }
+    });
+    Thread thread_2([&]() {
+      for (crutch::SizeType i = 0; i < 100; ++i) {
+        mutex.Lock();
+        number -= 1;
+        mutex.Unlock();
+      }
+    });
+
+    thread_1.Join();
+    thread_2.Join();
+
+    std::cout << std::boolalpha << (number == 100) << std::endl;
+  });
+  GetScheduler()->Dispatch();
+
+  std::cout << std::endl;
+}
+
+int main() {
+  YieldExample();
+  MutexExample();
+  int* a = new int(1);
+  a[5] = 1;
   return 0;
 }
